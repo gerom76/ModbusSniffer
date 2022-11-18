f6a5b63ca9d2ad2053bec2e150866aca470c197f
Your Name <you@example.com>
2022-11-18 01:02:57 +0100

    Fixed freq and QxEq settings

--- engine/chint666_adapter.py
+++ engine/chint666_adapter.py
@@ -9,9 +9,9 @@ logger = logging.getLogger()
 
 def process_meter_response(msg):
     try:
-        data = ' '.join([hex(i) for i in msg.registers])
+        data = ' '.join([str(i)+":"+hex(value) for i, value in enumerate(msg.registers)])
         count = len(msg.registers)
-        logger.info(f'Processing meter: {msg} ({count}) \n{data}\n{msg.registers}')
+        logger.info(f'Processing meter: {msg} ([{count}]) \n{data}\n{msg.registers}')
         if count == 60:
             logger.debug(f'Power data')
             power_data = decode_power(msg.registers)
@@ -24,16 +24,30 @@ def process_meter_response(msg):
             update_electricity(electricity_data)
         else:
             logger.debug(f'Unknown address')
-    except:
-        # logger.error(f'Error processing msg: {msg}')
+    except Exception as err:
+        logger.error(f'Error processing msg: {msg}: {err}')
         pass
     finally:
         logger.debug(f'Finished processing msg: {msg}')
 
+def log_decode_32bit_float(decoder: BinaryPayloadDecoder, count):
+    decoder.reset()
+    for i in range(count):
+        logger.info(f'{i}: {decoder.decode_32bit_float()},')
+    decoder.reset()
+
+def decode_range_32bit_float(decoder: BinaryPayloadDecoder, count):
+    decoder.reset()
+    data=[]
+    for i in range(count):
+        data.append(decoder.decode_32bit_float())
+    decoder.reset()
+    return data
 
 def decode_electricity(registers):
     decoder = BinaryPayloadDecoder.fromRegisters(
         registers, byteorder=Endian.Big, wordorder=Endian.Big)
+    regs = decode_range_32bit_float(decoder, int(len(registers)/2))
     dict = OrderedDict(
         [
             ("em_Uab", decoder.decode_32bit_float()*0.1), # 2000H -> 8192
@@ -55,48 +69,50 @@ def decode_electricity(registers):
             ("em_Qc", decoder.decode_32bit_float()*0.1), # 2020H -> 8224
         ]
     )
-    decoder.skip_bytes(16)
-    
-    # 202AH -> 8234
-    dict["em_PFt"] = decoder.decode_32bit_float()*0.001
-    dict["em_PFa"] = decoder.decode_32bit_float()*0.001
-    dict["em_PFb"] = decoder.decode_32bit_float()*0.001
-    
-    # 2030H -> 8240
-    dict["em_PFc"] = decoder.decode_32bit_float()*0.001
-    decoder.skip_bytes(20)
-    
-    # 2044H -> 8260
-    dict["em_Freq"] = decoder.decode_32bit_float()*0.01
 
+    decoder.skip_bytes(16)
+    # 202AH -> 8234
+    dict["em_PFt"] = decoder.decode_32bit_float()*0.001 # regs[17]*0.001
+    dict["em_PFa"] = decoder.decode_32bit_float()*0.001 # regs[18]*0.001
+    dict["em_PFb"] = decoder.decode_32bit_float()*0.001 # regs[19]*0.001
+    # 2030H -> 8240
+    dict["em_PFc"] = decoder.decode_32bit_float()*0.001 # regs[20]*0.001
+
+    # 2044H -> 8260
+    dict["em_Freq"] = regs[34]*0.01   # decoder.decode_32bit_float()*0.01
+
+    # log_decode_32bit_float(decoder,41)
     return dict
 
 
 def decode_power(registers):
     decoder = BinaryPayloadDecoder.fromRegisters(
         registers, byteorder=Endian.Big, wordorder=Endian.Big)
+    
+    regs = decode_range_32bit_float(decoder, int(len(registers)/2))
     dict = OrderedDict()
     # 101EH -> 4126
     dict["em_ImpEp"] = decoder.decode_32bit_float()
-    decoder.skip_bytes(10)
     
+    # decoder.skip_bytes(5)
     # 1028H -> 4136
-    dict["em_ExpEp"] = decoder.decode_32bit_float()
-    decoder.skip_bytes(10)
+    dict["em_ExpEp"] = regs[5]  # decoder.decode_32bit_float()
     
+    # decoder.skip_bytes(5)
     # 1032H -> 4146
-    dict["em_Q1Eq"] = decoder.decode_32bit_float()
-    decoder.skip_bytes(10)
+    dict["em_Q1Eq"] = regs[10]  # decoder.decode_32bit_float()
     
+    #decoder.skip_bytes(5)
     # 103CH -> 4156
-    dict["em_Q2Eq"] = decoder.decode_32bit_float()
-    decoder.skip_bytes(10)
+    dict["em_Q2Eq"] = regs[15]  # decoder.decode_32bit_float()
     
+    # decoder.skip_bytes(5)
     # 1046H -> 4166
-    dict["em_Q3Eq"] = decoder.decode_32bit_float()
-    decoder.skip_bytes(10)
+    dict["em_Q3Eq"] = regs[20]  # decoder.decode_32bit_float()
     
+    # decoder.skip_bytes(5)
     # 1050H -> 4176
-    dict["em_Q4Eq"] = decoder.decode_32bit_float()
+    dict["em_Q4Eq"] = regs[25]  # decoder.decode_32bit_float()
 
+    # log_decode_32bit_float(decoder,30)
     return dict
