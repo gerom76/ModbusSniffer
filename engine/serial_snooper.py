561890a8ce7268f6578dd6cd1b740d192f640497
Your Name <you@example.com>
2022-11-27 20:50:01 +0100

    Added common statistics

--- engine/serial_snooper.py
+++ engine/serial_snooper.py
@@ -1,8 +1,9 @@
 from collections import Counter, OrderedDict
+from datetime import datetime
 import logging
 import serial
 from pymodbus.factory import ClientDecoder, ServerDecoder
-from api.web_app import update_smart_meter, update_sniffing_quality
+from api.web_app import update_smart_meter, update_statistics
 from engine.chint666_adapter import Chint666LegacyAdapter, Chint666TunedAdapter
 from pymodbus.framer.rtu_framer import ModbusRtuFramer
 from pymodbus.utilities import (
@@ -20,6 +21,8 @@ class SerialSnooper:
     kByteLength = 10
     processedFramesCounter = 0
     interceptedResponseFramesCounter = 0
+    errorCounter = 0
+    lastError = ''
     
     def __init__(self, port, baud=9600, slave_address=1):
         logger.debug('SerialSnooper.__init__')
@@ -49,6 +52,24 @@ class SerialSnooper:
 
     def read_in_waiting(self):
         return self.serial.read(self.serial.in_waiting)
+
+    def reset_statistics(self):
+        self.processedFramesCounter = 0
+        self.interceptedResponseFramesCounter = 0
+        self.errorCounter = 0
+        
+    def get_statistics(self):
+        sniffingRate = 0
+        errorRate = 0
+        readingDate = datetime.now().strftime("%Y/%m/%d/ %H:%M:%S.%f")
+        if (self.processedFramesCounter>0):
+            sniffingRate = ((self.interceptedResponseFramesCounter) / self.processedFramesCounter) * 100
+            errorRate = ((self.errorCounter) / self.processedFramesCounter) * 100
+        
+        errorCounter = self.errorCounter
+        processedFramesCounter = self.processedFramesCounter
+        lastError = self.lastError
+        return readingDate, errorCounter, processedFramesCounter, errorRate, sniffingRate, lastError
 
 ###################################################
 # https://github.com/eterey/pymodbus3/blob/master/examples/contrib/message-parser.py
@@ -103,6 +124,7 @@ class SerialSnooper:
     # Optimized flow for string of interlaced request and responses:
     def run_method_optimized(self, slave_address):
         logger.warning(f"Starting Optimized Method")
+        self.reset_statistics()
         frameBuffer =  bytearray()
         isProcessingFrame1 = True
         frame1Request =  bytearray()
@@ -111,108 +133,122 @@ class SerialSnooper:
         frame2Response =  bytearray()
         total_data: OrderedDict = OrderedDict()
         start_address = ''
+        
         while True:
-            message = self.read_in_waiting()
-            if len(message) <= 0: continue
-            
-            logger.debug(f"message[{len(message)}]: {message.hex()}")
-            
-            if len(message)==8:
-                logger.debug(f"request (current): {message.hex()}")
-                if isProcessingFrame1:
-                    frame1Request = message
-                    frame2Response = frameBuffer
-                    isProcessingFrame1 = False
+            try:
+                self.processedFramesCounter += 1
+                if self.processedFramesCounter % 1000 == 0:
+                    readingDate, errorCounter, processedFramesCounter, errorRate, sniffingRate, lastError = self.get_statistics()
+                    update_statistics(readingDate, errorCounter, processedFramesCounter, errorRate, sniffingRate, lastError)
+                
+                message = self.read_in_waiting()
+                if len(message) <= 0: continue
+                
+                logger.debug(f"message[{len(message)}]: {message.hex()}")
+                
+                if len(message)==8:
+                    logger.debug(f"request (current): {message.hex()}")
+                    if isProcessingFrame1:
+                        frame1Request = message
+                        frame2Response = frameBuffer
+                        isProcessingFrame1 = False
+                    else:
+                        frame2Request = message
+                        frame1Response = frameBuffer
+                        isProcessingFrame1 = True
+                    frameBuffer = bytearray()
+                                        
+                    # logger.info(f"F1 {frame1Request.hex()} {frame1Response.hex()}")
+                    # logger.info(f"F2 {frame2Request.hex()} {frame2Response.hex()}")
+                    
+                    if isProcessingFrame1 and len(frame1Request)>0 and len(frame1Response)>0:
+                        start_address, data = self.process_optimized(frame1Request, frame1Response, slave_address)
+                    if not isProcessingFrame1 and len(frame2Request)>0 and len(frame2Response)>0:
+                        start_address, data = self.process_optimized(frame2Request, frame2Response, slave_address)
+
+                    if start_address=='2000':
+                        total_data = data
+                    elif start_address=='101e':
+                        total_data.update(data)
+                    
+                    if len(start_address)>0 and len(total_data)>22:
+                        logger.info(f'Ready to pass data: {total_data}')
+                        self.interceptedResponseFramesCounter = 0
+                        update_smart_meter(total_data)
+                        total_data = OrderedDict()
+
                 else:
-                    frame2Request = message
-                    frame1Response = frameBuffer
-                    isProcessingFrame1 = True
-                frameBuffer = bytearray()
-                                    
-                # logger.info(f"F1 {frame1Request.hex()} {frame1Response.hex()}")
-                # logger.info(f"F2 {frame2Request.hex()} {frame2Response.hex()}")
-                
-                if isProcessingFrame1 and len(frame1Request)>0 and len(frame1Response)>0:
-                    start_address, data = self.process_optimized(frame1Request, frame1Response, slave_address)
-                if not isProcessingFrame1 and len(frame2Request)>0 and len(frame2Response)>0:
-                    start_address, data = self.process_optimized(frame2Request, frame2Response, slave_address)
-
-                if start_address=='2000':
-                    total_data = data
-                elif start_address=='101e':
-                    total_data.update(data)
-                
-                if len(start_address)>0 and len(total_data)>22:
-                    logger.info(f'Ready to pass data: {total_data}')
-                    # TODO: Update data and statisticsy here every two seconds!
-                    update_smart_meter(total_data)
-                    total_data = OrderedDict()
-
-            else:
-                frameBuffer += message
+                    frameBuffer += message
+            except Exception as e:
+                logger.error(f'run_method_generic: error:{e}')
+                self.errorCounter += 1
+                self.lastError = e
+                pass
 
     def process_optimized(self, request, response, slave_address):
         logger.info(f'Processing request={request.hex()} response={response.hex()}')
-        try:
-            is_valid = SerialSnooper.check_message(request)
+        is_valid = SerialSnooper.check_message(request)
+        if is_valid:
+            slave_adr, func_code, start_address, quantity = SerialSnooper.decode_request_message(request)
+            
+            if slave_adr != slave_address: return
+            
+            is_valid = SerialSnooper.check_message(response)
             if is_valid:
-                slave_adr, func_code, start_address, quantity = SerialSnooper.decode_request_message(request)
-                
-                if slave_adr != slave_address: return
-                
-                is_valid = SerialSnooper.check_message(response)
-                if is_valid:
-                    slave_adr, func_code, byte_count = SerialSnooper.decode_response_message(response)
-                    logger.debug(f'Decoding response: func_code={func_code} start_address=0x{start_address} quantity=0x{quantity} byte_count={byte_count}')
-                    payload = SerialSnooper.extract_payload(response, byte_count)
-                    decoder = SerialSnooper.load_decoder(payload)
-                    SerialSnooper.log_registry(response, byte_count, decoder)
-                    chint666Adaper = Chint666TunedAdapter(decoder)
-                    if start_address=='2000':
-                        data = chint666Adaper.decode_electricity()
-                    elif start_address=='101e':
-                        data = chint666Adaper.decode_power()
-                    if data:
-                        return start_address, data
-                    else:
-                        logger.error(f'Invalid data: {data}')
-        except Exception as ex:
-            logger.error(f'Processing exception={ex} Request={request} Response={response}')
-            pass
+                slave_adr, func_code, byte_count = SerialSnooper.decode_response_message(response)
+                logger.debug(f'Decoding response: func_code={func_code} start_address=0x{start_address} quantity=0x{quantity} byte_count={byte_count}')
+                payload = SerialSnooper.extract_payload(response, byte_count)
+                decoder = SerialSnooper.load_decoder(payload)
+                SerialSnooper.log_registry(response, byte_count, decoder)
+                chint666Adaper = Chint666TunedAdapter(decoder)
+                if start_address=='2000':
+                    data = chint666Adaper.decode_electricity()
+                elif start_address=='101e':
+                    data = chint666Adaper.decode_power()
+                if data:
+                    return start_address, data
+                else:
+                    logger.error(f'Invalid data: {data}')
 
 ######################################################
 
     def run_method_generic(self, slave_address):
         read_size = 128
+        self.reset_statistics()
         logger.warning(f"Starting method Generic: read_size:{read_size}")
         while True:
-            data = self.read_raw(read_size)
-            self.process_generic(data, slave_address)
-            # time.sleep(float(1)/ss.baud)
+            try:
+                self.processedFramesCounter += 1
+                
+                if self.processedFramesCounter % 1000 == 0:
+                    readingDate, errorCounter, processedFramesCounter, errorRate, sniffingRate, lastError = self.get_statistics()
+                    update_statistics(readingDate, errorCounter, processedFramesCounter, errorRate, sniffingRate, lastError)
 
+                data = self.read_raw(read_size)
+                self.process_generic(data, slave_address)
+                # time.sleep(float(1)/ss.baud)
+            except Exception as e:
+                logger.error(f'run_method_generic: error:{e}')
+                self.errorCounter += 1
+                self.lastError = e
+                pass
+    
     def process_generic(self, data, slave_address):
         logger.debug(f'generic process: data={data.hex()}')
         if len(data) <= 0:
             return
-
-        self.processedFramesCounter += 1
-        try:
-            logger.debug("Check Client")
-            self.client_framer.processIncomingPacket(
-                data, self.slave_packet_callback, unit=slave_address, single=True)
-        except (IndexError, TypeError, KeyError) as e:
-            logger.error(e)
-            pass
-        try:
-            logger.debug("Check Server")
-            self.server_framer.processIncomingPacket(
-                data, self.master_packet_callback, unit=slave_address, single=True)
-            pass
-        except (IndexError, TypeError, KeyError) as e:
-            logger.error(e)
-            pass
-
-        update_sniffing_quality(self.get_statistics())
+        # try:
+        self.client_framer.processIncomingPacket(
+            data, self.slave_packet_callback, unit=slave_address, single=True)
+        # except (IndexError, TypeError, KeyError) as e:
+        #     logger.error(e)
+        #     pass
+        # try:
+        self.server_framer.processIncomingPacket(
+            data, self.master_packet_callback, unit=slave_address, single=True)
+        # except (IndexError, TypeError, KeyError) as e:
+        #     logger.error(e)
+        #     pass
 
     def master_packet_callback(self, *args, **kwargs):
         # logger.debug(f"responseBuffer: {self.responseBuffer.hex()}")
@@ -249,9 +285,3 @@ class SerialSnooper:
             arg += 1
             logger.info(f"Slave Response-> ID: {msg.unit_id}, arg({arg}/{len(args)}) Function: {func_name}: {msg.function_code}")
             self.chint666LegacyAdapter.process_meter_response(msg)
-
-    def get_statistics(self):
-        if (self.processedFramesCounter==0):
-            return 0
-        return ((self.interceptedResponseFramesCounter) / self.processedFramesCounter) * 100
-
