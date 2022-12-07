c10c91681166c132e3104a9a584f09df3bb42653
Your Name <you@example.com>
2022-12-07 21:33:47 +0100

    Enabled waveshare dongle

--- modbus-sniffer.sh
+++ modbus-sniffer.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 rm logs/modbus_sniffer.log
-python modbus_sniffer.py /dev/ttyUSB0 9600 1 generic
+# Cheap USB RS485 dongle
+# python modbus_sniffer.py /dev/ttyUSB0 9600 1 generic
+# Waveshare USB RS485 B dongle
+python modbus_sniffer.py /dev/ttyACM0 9600 1 generic
