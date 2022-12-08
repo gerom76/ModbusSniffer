ee1a4ee7c6560215e99e366cdd7dcf9f1243e4f9
Your Name <you@example.com>
2022-12-08 22:23:28 +0100

    longer wait

--- engine/SerialSnooper.py
+++ engine/SerialSnooper.py
@@ -152,7 +152,7 @@ class SerialSnooper:
         rt.start()
         while True:
             try:
-                time.sleep(float(10)/self.baud) # to reduce cpu stress
+                time.sleep(float(500)/self.baud) # to reduce cpu stress
                 message = self.read_in_waiting()
                 if len(message) <= 0: continue
                 self.processedFramesCounter += 1
